78a4945fc72cbba647f29617f084813e6a8961f3
Eric StJohn <ericstj@microsoft.com>
2024-12-19 14:01:22 -0800

    Dreaming :)

---
+++ improved-dev-dependencies/task/MyTask.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace task;
+public class MyTask : Microsoft.Build.Utilities.Task
+{
+    public override bool Execute()
+    {
+        string[] message = [ "Hello", "From", "Task" ];
+        // use JsonSerializer to serialize message
+        var json = JsonSerializer.Serialize(message);
+        Log.LogMessage(MessageImportance.High, json);
+        
+        return true;
+    }
+
+}
